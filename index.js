2532f07eecf787745930e26acf754f02a384a2cc
xiangshouding <fansekey@gmail.com>
2015-08-12 14:50:28 +0800

    fix

--- index.js
+++ index.js
@@ -12,7 +12,17 @@ module.exports = function (options, modified, total, next) {
   modified.forEach(function (file) {
     if (file.isText() || typeof (file.getContent()) === 'string') {
       var content = file.getContent();
+
+      if (fis.util.is(options.from, 'String')) {
+        options.from = new RegExp(fis.util.escapeReg(options.from), 'g');
+      }
+
+      if (!fis.util.is(options.from, 'RegExp')) {
+        fis.log.error('fis3-deploy-replace: option.from must a string or RegExp.');
+      }
+
       var result = content.replace(options.from, options.to);
+
       file.setContent(result);
       if (result !== content) {
         fis.log.debug('Replace from %s to %s in file [%s]', options.from, options.to, file);
